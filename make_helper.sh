e249c7a2839d249e4f0e3a12261d852788716338
Patrick Boivin <pboivin@gmail.com>
2018-08-17 20:15:03 -0400

    Generate snippet for .bashrc using make_helper.sh

--- make_helper.sh
+++ make_helper.sh
@@ -1,7 +1,9 @@
-alias fuzzycd="python $PWD/fuzzycd.py"
+echo '
+FUZZYCD_PYTHON="python"
+FUZZYCD_SCRIPT="'$PWD'/fuzzycd.py"
 
 function f() {
-    local output=$(fuzzycd $@)
+    local output=$($FUZZYCD_PYTHON $FUZZYCD_SCRIPT $@)
     local split=($output)
     if [ "${split[0]}" == "found:" ]; then
         unset split[0]
@@ -10,3 +12,4 @@ function f() {
         echo "$output"
     fi
 }
+'
