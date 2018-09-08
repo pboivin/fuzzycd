3571847781ddb5c9abf2248b5018384e66c0eef6
Patrick Boivin <pboivin@gmail.com>
2018-09-08 14:02:28 -0400

    Tests for path_is_directory

--- test.py
+++ test.py
@@ -13,15 +13,39 @@ class TestFuzzyCD(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.temp_dir = tempfile.mkdtemp()
+        # Make directory
+        cls.temp_dir_path = tempfile.mkdtemp()
+
+        # Make directory link
+        cls.temp_dir_path_link = cls.temp_dir_path + "_link"
+        os.symlink(cls.temp_dir_path, cls.temp_dir_path_link)
+
+        # Make regular file
+        temp_file, temp_file_path = tempfile.mkstemp()
+        cls.temp_file_path = temp_file_path
 
     @classmethod
     def tearDownClass(cls):
-        if cls.temp_dir:
-            shutil.rmtree(cls.temp_dir)
+        if cls.temp_dir_path:
+            shutil.rmtree(cls.temp_dir_path)
+        if cls.temp_dir_path_link:
+            os.remove(cls.temp_dir_path_link)
+        if cls.temp_file_path:
+            os.remove(cls.temp_file_path)
 
     def test_path_is_directory(self):
-        self.assertTrue(path_is_directory(self.temp_dir))
+        self.assertTrue(path_is_directory(self.temp_dir_path))
+
+    def test_path_is_directory_reject_file(self):
+        self.assertFalse(path_is_directory(self.temp_file_path))
+
+    def test_path_is_directory_accept_symlink(self):
+        self.assertTrue(
+            path_is_directory(self.temp_dir_path_link, follow_links=True))
+
+    def test_path_is_directory_reject_symlink(self):
+        self.assertFalse(
+            path_is_directory(self.temp_dir_path_link, follow_links=False))
 
 
 if __name__ == "__main__":
